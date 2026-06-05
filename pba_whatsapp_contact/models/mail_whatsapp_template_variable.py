c7f080b6ecc36e9cc4e4f8718afce46e6b3615c8
andyengit <anderson.armeya@gmail.com>
2026-06-05 07:26:03 +0000

    Increment version of PBA WhatsApp Contacto module to 18.0.1.0.20, update model fields in mail template variable and mail template models for improved clarity. The 'model' field is now defined separately, and 'model_name' is updated to 'Modelo técnico' for better understanding of the data structure.

--- pba_whatsapp_contact/models/mail_whatsapp_template_variable.py
+++ pba_whatsapp_contact/models/mail_whatsapp_template_variable.py
@@ -48,7 +48,8 @@ class MailWhatsappTemplateVariable(models.Model):
     model_id = fields.Many2one(
         related="template_id.model_id",
     )
-    model_name = fields.Char(related="model_id.model", string="Modelo")
+    model = fields.Char(related="model_id.model", string="Modelo")
+    model_name = fields.Char(related="model_id.model", string="Modelo técnico")
 
     @api.depends("source_type", "field_id", "static_value", "position")
     def _compute_sample_value(self):
